2fe919378a220623f6194730479a0a95da40ba6e
rominaserrano <rominaserrano94@gmail.com>
2022-09-30 21:35:54 -0300

    avance 1

--- JAPFLIX.js
+++ JAPFLIX.js
@@ -1,88 +1,19 @@
-const  MOVIES_DATA = "https://japceibal.github.io/japflix_api/movies-data.json";
-var buscar = undefined;
-let currentMoviesArray = [];
-
-let showSpinner = function(){
-    document.getElementById("spinner-wrapper").style.display = "block";
-  }
-  
-  let hideSpinner = function(){
-    document.getElementById("spinner-wrapper").style.display = "none";
-  }
-  
-  let getJSONData = function(url){
-      let result = {};
-      showSpinner();
-      return fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }else{
-          throw Error(response.statusText);
-        }
-      })
-      .then(function(response) {
-            result.status = 'ok';
-            result.data = response;
-            hideSpinner();
-            return result;
-      })
-      .catch(function(error) {
-          result.status = 'error';
-          result.data = error;
-          hideSpinner();
-          return result;
-      });
-  }
-  document.addEventListener("DOMContentLoaded", function(e){
+//CÓDIGO A EJECUTARSE CUANDO EL DOM SE HA CARGADO
+document.addEventListener("DOMContentLoaded", function(){
+   
+  //FETCHING DE LAS PELÍCULAS
+  getJSONData(MOVIES_DATA).then(function(resultObj){
+      if (resultObj.status === "ok"){
+          currentMoviesArray = resultObj.data
+          console.log(currentMoviesArray)
+         
+      }
   });
 
-
-
-  function showMoviesList(){
-
-   
-    let htmlContentToAppend = "";
-
-    for (let i = 0; i < currentMoviesArray.length; i++) {
-        let movie = array[i];
-
-        if  ((buscar != undefined) 
-            && (movie.title.toLowerCase().includes(buscar.toLowerCase())
-            || movie.genres.toLowerCase().includes(buscar.toLowerCase())
-            || movie.tagline.toLowerCase().includes(buscar.toLowerCase())
-            || movie.overview.toLowerCase().includes(buscar.toLowerCase()))) {
-            
-
-            htmlContentToAppend +=
-                `<div class="album">
-                  <div onclick="${movie.id}" class="card" "${movie.title}">
-                  <div class="card-body">
-                    <h5 class="card-">${movie.title} + "${movie.vote_average}"</h5>
-                    <p class="card-text">${movie.overview} </div> </div> </div> `
-
-        document.getElementById("lista").innerHTML = htmlContentToAppend;
-            }
-        else {
-           alert("No se registra la existencia de película")
-        }
-    }
-
-}
-        
-  
-  document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(MOVIES_DATA).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            currentMoviesArray = resultObj.data
-            showMoviesList()
-        }
-    })
-})
-
-document.getElementById("btnBuscar").addEventListener("click", function () {
-
-    buscar = document.getElementById("btnBuscar").value;
-    showMoviesList();
+  //CLICK EN EL BOTÓN BUSCAR
+  document.getElementById("btnBuscar").addEventListener("click", function () {
+      buscar = document.getElementById("inputBuscar").value;
+      showMoviesList();
+  });
 
 });
---
+++ init.js
@@ -0,0 +1,79 @@
+//INIT VARIABLES Y CONSTANTES
+const  MOVIES_DATA = "https://japceibal.github.io/japflix_api/movies-data.json";
+let currentMoviesArray = [];
+ 
+//DEFINIMOS LA FUNCIÓN getJSONData
+let getJSONData = function(url){
+      let result = {};
+     
+      return fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }else{
+          throw Error(response.statusText);
+        }
+      })
+      .then(function(response) {
+            result.status = 'ok';
+            result.data = response;
+           
+            return result;
+      })
+      .catch(function(error) {
+          result.status = 'error';
+          result.data = error;
+ 
+          return result;
+      });
+  }
+ 
+ 
+  function stars(){
+    let puntos = Math.round((movie.vote_average)/2)
+    var estrellitas = "";
+
+    for (let i = 0; i < 5; i++) {
+        if (i < puntos) {
+        estrellitas += `<span class="fa fa-star checked"></span>`;
+        } else {
+            estrellitas += `<span class="fa fa-star"></span>`;
+        }
+    }
+
+    return estrellitas;
+}
+//DEFINIMOS FUNCIÓN PARA AGREGAR LISTA DE PELÍCULAS AL HTML DEL INDEX
+function showMoviesList(){
+                     
+    let htmlContentToAppend = "";
+   
+    for (let i = 0; i < currentMoviesArray.length; i++) {
+        let movie = currentMoviesArray[i];
+       
+        if  ((buscar != "")
+        && (movie.title.toLowerCase().includes(buscar.toLowerCase())
+        || movie.tagline.toLowerCase().includes(buscar.toLowerCase())
+        || movie.overview.toLowerCase().includes(buscar.toLowerCase()))) {
+           
+           
+            htmlContentToAppend +=
+            `<div class="album">
+            <div onclick="${movie.id}" class="card" "${movie.title}">
+            <div class="card-title">
+            <h4  style="margin-left:20px; display:inline; float:left">${movie.title} `+  
+             ` <h4 style="margin-right:20px; margin-right:40px; display:inline; float:right">${movie.vote_average/2}</h5> 
+             <br>
+            <p class="card-body">${movie.overview} </div> </div> </div> `
+           
+       
+        }
+    }
+ 
+    if (htmlContentToAppend !=""){
+        document.getElementById("lista").innerHTML = htmlContentToAppend;
+    }
+    else{
+        alert("No se registra la existencia de película");
+    }
+}   
